01d78f2cdb42b64a09ab50a0cab430c0a2832b08
Batkhuleg Bazarragchaa <khulegkhan@gmail.com>
2025-05-25 23:33:47 -0400

    add some styles

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,8 @@
 import ChessboardContainer from "@/components/ChessboardContainer";
-
+// 3c1d0e
 const HomePage = () => {
   return (
-    <div className="w-full flex justify-center">
+    <div className="w-full min-h-screen bg-[#310d04] flex justify-center items-center">
       <ChessboardContainer />
     </div>
   );
